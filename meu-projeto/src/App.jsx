25bb72dce7b83b51a92b4fcdb3846c33da410bcd
gori2016 <igorborgesaraujo1677@gmail.com>
2025-12-09 18:37:32 -0300

    Estruturação do projeto.

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -1,17 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import Container from './components/layout/Container'
+import Home from './components/pages/Home'
+import Contato from './components/pages/Contato'
+import NewProject from './components/pages/NewProject'
+import Company from './components/pages/Company'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-     <div>
-      <h1>teste</h1>
-     </div>
-    </>
+    <Router>
+      <Container customClass="min-height">
+        
+        <nav>
+          <Link to="/">Home</Link> |
+          <Link to="/contato">Contato</Link> |
+          <Link to="/newProject">New Project</Link> |
+          <Link to="/company">Company</Link>
+        </nav>
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/contato" element={<Contato />} />
+          <Route path="/newProject" element={<NewProject />} />
+          <Route path="/company" element={<Company />} />
+        </Routes>
+
+      </Container>
+      <footer><p>AOA</p></footer>
+    </Router>
   )
 }
 
---
+++ meu-projeto/src/components/pages/NewProject.jsx
@@ -0,0 +1,9 @@
+function NewProject(){
+    return(
+        <div>
+            <h1>NewProject Page</h1>
+        </div>
+    )
+}
+
+export default NewProject
