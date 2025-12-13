eba02b94314a41ef890df331f209125acc223cfd
gori2016 <igorborgesaraujo1677@gmail.com>
2025-12-13 16:00:18 -0300

    Adiciona componente ProjectCard e implementa busca de projetos na página de projetos

--- meu-projeto/src/components/pages/Projects.jsx
+++ meu-projeto/src/components/pages/Projects.jsx
@@ -3,25 +3,48 @@ import { useLocation } from "react-router-dom"
 import styles from "./Projects.module.css"
 import Container from "../layout/Container"
 import LinkButtom from "../layout/LinkButtom"
+import ProjectCard from "../projects/ProjectCard"
+import { useState, useEffect } from "react"
 
 function Projects() {
-    const location = useLocation()
-    let mensage = ""
-    if (location.state) {
-        mensage = location.state.message
-    }
-    return (
-        <div className={styles.project_container}>
-            <div className={styles.title_container}>
-                <h1>Meus Projetos</h1>
-                <LinkButtom to="/newproject" text="Criar Projeto" />
-            </div>
-            {mensage && <Message type="success" msg={mensage} />}
-            <Container customClass="start">
-                <p>Projetos</p>
-            </Container>
-        </div>
-    )
+  const [projects, setProjects] = useState([])
+
+  const location = useLocation()
+  let mensage = ""
+
+  if (location.state) {
+    mensage = location.state.message
+  }
+
+  useEffect(() => {
+    fetch("http://localhost:5000/projects", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        console.log(data)
+        setProjects(data)
+      })
+      .catch((err) => console.log(err))
+  }, [])
+
+  return (
+    <div className={styles.project_container}>
+      <div className={styles.title_container}>
+        <h1>Meus Projetos</h1>
+        <LinkButtom to="/newproject" text="Criar Projeto" />
+      </div>
+
+      {mensage && <Message type="success" msg={mensage} />}
+
+      <Container customClass="start">
+        <p>Projetos...</p>
+      </Container>
+    </div>
+  )
 }
 
-export default Projects 
+export default Projects
---
+++ meu-projeto/src/components/projects/ProjectCard.jsx
@@ -0,0 +1,11 @@
+import styles from './ProjectCard.module.css';
+import { BsFillTrashFill, BsPencil } from 'react-icons/bs';
+function ProjectCard({id, name, budget, category, handleRemove}) {
+    return(
+        <div>
+
+        </div>
+    )
+}
+
+export default ProjectCard;
